1fd3797cfbd575ee0814b0434e845fd55b6a2cce
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2010-04-13 06:12:17 +0000

    update of start and stop scripts for generalised values.

--- brihaspati/trunk/brihaspati2/bin/BrihStartRoot.sh
+++ brihaspati/trunk/brihaspati2/bin/BrihStartRoot.sh
@@ -5,20 +5,25 @@
 # Please don't use this as Brihaspati to start the system.
 # Instead use BrihStart.sh
 
-JAVA_HOME=/usr/java/jdk1.5.0_02
-PATH=/usr/java/jdk1.5.0_02/bin:$PATH
+JAVA_HOME=/usr/java/jdk1.6.0_02
+PATH=/usr/java/jdk1.6.0_02/bin:$PATH
+export JAVA_HOME PATH
 
-ANT_HOME=~/apache-ant-1.6.1
-PATH=~/apache-ant-1.6.1/bin:$PATH
-BRIHASPATI_HOME=/home/brihaspati
-SUDO_HOME=/usr/bin
-export PATH JAVA_HOME ANT_HOME BRIHASPATI_HOME SUDO_HOME
+BRIHASPATI_HOME=/home/brihaspati 
+#ANT_HOME=$BRIHASPATI_HOME/apache-ant-1.6.1
+#PATH=$BRIHASPATI_HOME/apache-ant-1.6.1/bin:$PATH
+
+#SUDO_HOME=/usr/bin
 cd $BRIHASPATI_HOME/tdk-2.3_01/webapps/brihaspati2/babylon
 
-$SUDO_HOME/sudo -u brihaspati $BRIHASPATI_HOME/tdk-2.3_01/bin/startup.sh
+$BRIHASPATI_HOME/tdk-2.3_01/bin/startup.sh
 
 CLASSPATH=$CLASSPATH:$BRIHASPATI_HOME/tdk-2.3_01/webapps/brihaspati2/babylon/babylon.jar
+
 export CLASSPATH
-$SUDO_HOME/sudo -u brihaspati java babylon.babylonServer -nographics -chatlogs -usepasswords &
-#$SUDO_HOME/sudo -u brihaspati $BRIHASPATI_HOME/tdk-2.3_01/webapps/brihaspati2/bin/brandgang.sh &
+java babylon.babylonServer -nographics -chatlogs -usepasswords &
+#$SUDO_HOME/sudo -u ynsingh $BRIHASPATI_HOME/tdk-2.3_01/webapps/brihaspati2/bin/brandgang.sh &
+
+#export PATH JAVA_HOME ANT_HOME BRIHASPATI_HOME SUDO_HOME CLASSPATH
+
 
--- brihaspati/trunk/brihaspati2/bin/BrihStop.sh
+++ brihaspati/trunk/brihaspati2/bin/BrihStop.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-BRIHASPATI_HOME=/home/shaista
+BRIHASPATI_HOME=/home/brihaspati
 
-$BRIHASPATI_HOME/New_tdk-2.3_01/bin/shutdown.sh
-CLASSPATH=$CLASSPATH:$BRIHASPATI_HOME/New_tdk-2.3_01/webapps/brihaspati2/babylon/babylon.jar
-
-#CLASSPATH=$CLASSPATH:$BRIHASPATI_HOME
+cd $BRIHASPATI_HOME/tdk-2.3_01/webapps/brihaspati2/babylon
+$BRIHASPATI_HOME/tdk-2.3_01/bin/shutdown.sh
+CLASSPATH=$CLASSPATH:$BRIHASPATI_HOME/tdk-2.3_01/webapps/brihaspati2/babylon/babylon.jar
 export CLASSPATH
-##cd $BRIHASPATI_HOME/New_tdk-2.3_01/webapps/brihaspati2/babylon
 java babylon.babylonServerStop STOP & 
