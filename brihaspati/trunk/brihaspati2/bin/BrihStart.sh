3c3aad4375ffffcfcbe6d92d574a63f2b25759cd
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2010-04-13 06:20:08 +0000

    update of start script for generalised values.

--- brihaspati/trunk/brihaspati2/bin/BrihStart.sh
+++ brihaspati/trunk/brihaspati2/bin/BrihStart.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-BRIHASPATI_HOME=/home/shaista
+BRIHASPATI_HOME=/home/brihaspati
 
-cd $BRIHASPATI_HOME/New_tdk-2.3_01/webapps/brihaspati2/babylon
-$BRIHASPATI_HOME/New_tdk-2.3_01/bin/startup.sh
-CLASSPATH=$CLASSPATH:$BRIHASPATI_HOME/New_tdk-2.3_01/webapps/brihaspati2/babylon/babylon.jar
+cd $BRIHASPATI_HOME/tdk-2.3_01/webapps/brihaspati2/babylon
+$BRIHASPATI_HOME/tdk-2.3_01/bin/startup.sh
+CLASSPATH=$CLASSPATH:$BRIHASPATI_HOME/tdk-2.3_01/webapps/brihaspati2/babylon/babylon.jar
 export CLASSPATH
-java java babylon.babylonServerStart -nographics -chatlogs -usepasswords & 
-#$BRIHASPATI_HOME/tdk-2.3_01/webapps/brihaspati2/bin/brandgang.sh &
+java babylon.babylonServerStart -nographics -chatlogs -usepasswords & 
