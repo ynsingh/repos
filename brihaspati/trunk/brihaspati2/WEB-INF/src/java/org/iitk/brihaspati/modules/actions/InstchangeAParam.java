f66929ca6be02a6b2d03efd044aedb2b7d5d254f
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2010-11-16 07:42:13 +0000

    Suneel: Institute name wise InstAdmin Profile generation and update.

---
+++ brihaspati/trunk/brihaspati2/WEB-INF/src/java/org/iitk/brihaspati/modules/actions/InstchangeAParam.java
@@ -0,0 +1,161 @@
+package org.iitk.brihaspati.modules.actions;
+
+/*
+ * @(#)InstchangeAParam.java	
+ *
+ *  Copyright (c) 2010 ETRG,IIT Kanpur. 
+ *  All Rights Reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or 
+ *  without modification, are permitted provided that the following 
+ *  conditions are met:
+ * 
+ *  Redistributions of source code must retain the above copyright  
+ *  notice, this  list of conditions and the following disclaimer.
+ * 
+ *  Redistribution in binary form must reproducuce the above copyright 
+ *  notice, this list of conditions and the following disclaimer in 
+ *  the documentation and/or other materials provided with the 
+ *  distribution.
+ * 
+ * 
+ *  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED.  IN NO EVENT SHALL ETRG OR ITS CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL,SPECIAL, EXEMPLARY, OR 
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ *  OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+ *  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+ *  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+ *  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  Contributors: Members of ETRG, I.I.T. Kanpur
+ */
+import java.io.File;
+import org.apache.turbine.Turbine;
+import org.apache.turbine.util.RunData;
+import org.apache.velocity.context.Context;
+import org.apache.turbine.om.security.User;
+import org.iitk.brihaspati.modules.utils.AdminProperties;
+import org.apache.turbine.util.parser.ParameterParser;
+import org.iitk.brihaspati.modules.utils.StringUtil;
+import org.iitk.brihaspati.modules.utils.MultilingualUtil;
+import org.iitk.brihaspati.modules.utils.QuotaUtil;
+import org.iitk.brihaspati.modules.utils.ErrorDumpUtil;
+import org.apache.turbine.services.security.TurbineSecurity;
+
+/**
+ * @author <a href="mailto:sunil.singh6094@gmail.com">Sunil Kumar</a>
+ */
+
+//public class InstchangeAParam extends SecureAction_Admin{
+public class InstchangeAParam extends SecureAction{
+
+	/**
+	 * This method updates the first, last name and configuration 
+	 * parameters of Institute Admin
+	 * @see MultilingualUtil in  utils
+	 * @param data RunData
+	 * @param context Context
+	 * @return nothing
+	 */
+	private String LangFile=null;
+	MultilingualUtil m_u= new MultilingualUtil();
+	public void doUpdate(RunData data, Context context) throws Exception{
+		/**
+		 * Get the user object from RunData for the user
+		 * currently logged in
+		 */
+
+		User user=data.getUser();
+
+		/**
+                 * getting property file According to selection of Language in temporary variable 
+		 * getting the values of first,last names and
+		 * configuration parameter.
+		 */
+
+		LangFile=(String)user.getTemp("LangFile");
+		ParameterParser pp=data.getParameters();
+		//Institute Name
+		String iname=pp.getString("iname","");
+                context.put("iname",iname);
+		//Admin First Name
+		String AFName=pp.getString("AFName","");
+		//Admin Last Name
+		String ALName=pp.getString("ALName","");
+		//List config Value
+	 	String AdminConf = pp.getString("AdminConf","");	
+		//Course Expiry Time
+	 	String AdminCrsExp = pp.getString("AdminCrsExp","");	
+		//Local Doamin Name	
+	 	String domainNM = pp.getString("mailDomain","");	
+		//Email
+	 	String eMail = pp.getString("eMail","");	
+		//Course Quota
+	 	String aquota = pp.getString("cquota","");
+		//User Quota
+	 	String uquota = pp.getString("uquota","");
+		//Institute Quota
+	 	String iquota = pp.getString("iquota","");
+		//Faq Expiry time in days
+		String AdminFaqExp=pp.getString("AdminFaqExp","");
+		//Home Directory
+	 	String hdir = pp.getString("hdir","");
+		/**
+		 * Replacing the variable value from Property file
+		 * Update the first,last name configuration parameter values for Institute Admin
+		 * @see InstituteName+Admin.Properties in utils
+		 */
+		//iname=Institute name
+		String path="";	
+		path=data.getServletContext().getRealPath("/WEB-INF")+"/conf"+"/"+iname+"Admin.properties";
+		StringUtil S = new StringUtil();
+		String prof_update=null;
+		if (S.checkString(AFName)==-1 && S.checkString(ALName)==-1){
+			user.setFirstName(AFName);
+			user.setLastName(ALName);
+			user.setEmail(eMail);
+			TurbineSecurity.saveUser(user);
+			// for delete the file  and set the value for admin configuration
+		 	(new File(path)).delete();
+			AdminProperties.setValue(path,AdminConf,"brihaspati.admin.listconfiguration.value");
+			AdminProperties.setValue(path,AdminCrsExp,"brihaspati.admin.courseExpiry");
+			AdminProperties.setValue(path,domainNM,"brihaspati.mail.local.domain.name");
+			AdminProperties.setValue(path,aquota,"brihaspati.admin.quota.value");
+			AdminProperties.setValue(path,uquota,"brihaspati.user.quota.value");
+			AdminProperties.setValue(path,iquota,"brihaspati.user.iquota.value");
+			AdminProperties.setValue(path,hdir,"brihaspati.home.dir.value");
+			AdminProperties.setValue(path,AdminFaqExp,"brihaspati.admin.FaqExpiry");
+			prof_update=m_u.ConvertedString("usr_prof",LangFile);
+			data.setMessage(prof_update);
+			boolean qct=QuotaUtil.CreateandUpdate();	
+			//data.addMessage("Disk space is update for user and Courses");
+			data.addMessage(m_u.ConvertedString("qmgmt_msg6",LangFile));
+			}
+		else
+			prof_update=m_u.ConvertedString("usr_prof1",LangFile);
+			data.setMessage(prof_update);
+	}	
+
+	/**
+	 * This is the default method called when the action is not
+	 * found
+	 * @param data RunData
+	 * @param context Context
+	 */
+
+	public void doPerform(RunData data, Context context) throws Exception{
+		String action=data.getParameters().getString("actionName","");
+		LangFile=(String)data.getUser().getTemp("LangFile");
+		if(action.equals("eventSubmit_doUpdate"))
+			doUpdate(data,context);
+		else
+		{
+			String msg=MultilingualUtil.ConvertedString("usr_prof2",LangFile);
+			data.setMessage(msg);
+		}
+	}
+}
---
+++ brihaspati/trunk/brihaspati2/WEB-INF/src/java/org/iitk/brihaspati/modules/screens/call/AdminProfile/InstituteProfile.java
@@ -0,0 +1,98 @@
+package org.iitk.brihaspati.modules.screens.call.AdminProfile;
+
+/*
+ * @(#)InstituteProfile.java	
+ *  Copyright (c) 2010 ETRG,IIT Kanpur. 
+ *  All Rights Reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or 
+ *  without modification, are permitted provided that the following 
+ *  conditions are met:
+ * 
+ *  Redistributions of source code must retain the above copyright  
+ *  notice, this  list of conditions and the following disclaimer.
+ * 
+ *  Redistribution in binary form must reproducuce the above copyright 
+ *  notice, this list of conditions and the following disclaimer in 
+ *  the documentation and/or other materials provided with the 
+ *  distribution.
+ *  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED.  IN NO EVENT SHALL ETRG OR ITS CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL,SPECIAL, EXEMPLARY, OR 
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ *  OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+ *  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+ *  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+ *  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  
+ *  Contributors: Members of ETRG, I.I.T. Kanpur 
+ */
+import org.iitk.brihaspati.modules.utils.AdminProperties;
+import org.iitk.brihaspati.modules.utils.MultilingualUtil;
+import org.iitk.brihaspati.modules.screens.call.SecureScreen;
+import org.apache.turbine.om.security.User;
+import org.apache.turbine.util.RunData;
+import org.apache.velocity.context.Context;
+import org.iitk.brihaspati.modules.utils.ErrorDumpUtil;
+
+/**
+ * @author <a href="mailto:sunil.singh6094@gmail.com">Sunil Kumar</a> 
+ */
+
+/**
+ * Loads the template page for Institute Administrator
+ */
+
+//public class InstituteProfile extends SecureScreen
+public class InstituteProfile extends SecureScreen{
+	public void doBuildTemplate(RunData data, Context context){
+		User user = data.getUser();
+		//Get Institute name(iname)	
+		String iname=data.getParameters().getString("iname","");
+                context.put("iname",iname);
+		//set path InstituteName+Admin.properies
+		String path="";	
+		path=data.getServletContext().getRealPath("/WEB-INF")+"/conf"+"/"+iname+"Admin.properties";
+		
+		String LangFile=data.getUser().getTemp("LangFile").toString();
+		context.put("tdcolor",data.getParameters().getString("count",""));
+		try{
+		/**
+		 * getting value of configuration parameter 	
+		 * @see AdminProperties in utils
+		 */
+		 String AdminConf = AdminProperties.getValue(path,"brihaspati.admin.listconfiguration.value");
+		 context.put("AdminConf",new Integer(AdminConf));
+		 String CrsExp = AdminProperties.getValue(path,"brihaspati.admin.courseExpiry");
+		 context.put("CrsExp",new Integer(CrsExp));
+		 String domainNm = AdminProperties.getValue(path,"brihaspati.mail.local.domain.name");
+		 context.put("dName",domainNm);
+		 String email = AdminProperties.getValue(path,"brihaspati.mail.email");
+		 context.put("eMail","");
+		 String cquota = AdminProperties.getValue(path,"brihaspati.admin.quota.value");
+		 context.put("cquota",cquota);
+		 String uquota = AdminProperties.getValue(path,"brihaspati.user.quota.value");
+		 context.put("uquota",uquota);
+		 String iquota = AdminProperties.getValue(path,"brihaspati.user.iquota.value");
+		 context.put("iquota",iquota);
+		 String hdir = AdminProperties.getValue(path,"brihaspati.home.dir.value");
+		 if(hdir.equals("")){
+			hdir=System.getProperty("user.home");
+		 }
+		 context.put("hdir",hdir);
+                 String FaqExp = AdminProperties.getValue(path,"brihaspati.admin.FaqExpiry");
+                 context.put("FaqExp",new Integer(FaqExp));
+		}
+		catch(Exception e) {	
+			data.addMessage(MultilingualUtil.ConvertedString("adm_msg1",LangFile)); 
+			//data.addMessage("Some Problem Occured in getting the Parameter Value"); 
+		}
+		context.put("afname",user.getFirstName());
+		context.put("alname",user.getLastName());	
+		context.put("eMail",user.getEmail());	
+	}
+}
+
