ad7244205f7136edd0816db41319c2e269b9c500
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2011-03-31 12:33:44 +0000

    Ashish: extra directory removed, mouse event capturing, and proxy related code now added.

--- brihaspati/trunk/brihaspatisync_client/src/java/org/bss/brihaspatisync/network/desktop_sharing/GetSharedScreen.java
+++ brihaspati/trunk/brihaspatisync_client/src/java/org/bss/brihaspatisync/network/desktop_sharing/GetSharedScreen.java
@@ -18,6 +18,15 @@ import java.awt.image.BufferedImage;
 import org.bss.brihaspatisync.gui.Desktop_Sharing;
 import org.bss.brihaspatisync.util.ClientObject;
 
+import org.bss.brihaspatisync.util.RuntimeDataObject;
+
+import org.apache.commons.httpclient.auth.AuthScope;
+
+import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.HostConfiguration;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+
+
 /**
  * @author <a href="mailto: arvindjss17@gmail.com" > Arvind Pal </a>
  * @author <a href="mailto: ashish.knp@gmail.com" > Ashish Yadav</a>
@@ -30,7 +39,7 @@ public class GetSharedScreen implements Runnable {
 	private boolean flag=false;
 
 	private ClientObject clientObject=ClientObject.getController();
-	
+	private RuntimeDataObject runtime_object=RuntimeDataObject.getController();
 	private static GetSharedScreen get_screen=null;
 
 	public static GetSharedScreen getController(){
@@ -72,6 +81,15 @@ public class GetSharedScreen implements Runnable {
 				HttpMethod method = new GetMethod("http://"+clientObject.getReflectorIP()+":8883");
 		                client.setConnectionTimeout(8000);
                                 method.setRequestHeader("Content-type","image/jpeg; charset=ISO-8859-1");
+				// Http Proxy Handler
+				if((!(runtime_object.getProxyHost()).equals("")) && (!(runtime_object.getProxyPort()).equals(""))){
+                                        HostConfiguration config = client.getHostConfiguration();
+                                        config.setProxy(runtime_object.getProxyHost(),Integer.parseInt(runtime_object.getProxyPort()));
+                                        Credentials credentials = new UsernamePasswordCredentials(runtime_object.getProxyUser(), runtime_object.getProxyPass());
+                                        AuthScope authScope = new AuthScope(runtime_object.getProxyHost(), Integer.parseInt(runtime_object.getProxyPort()));
+                                        client.getState().setProxyCredentials(authScope, credentials);
+                                }
+
                                 int statusCode1 = client.executeMethod(method);
                                 byte[] bytes1=method.getResponseBody();
                                 BufferedImage image = ImageIO.read(new ByteArrayInputStream(bytes1));
--- brihaspati/trunk/brihaspatisync_client/src/java/org/bss/brihaspatisync/network/desktop_sharing/PostSharedScreen.java
+++ brihaspati/trunk/brihaspatisync_client/src/java/org/bss/brihaspatisync/network/desktop_sharing/PostSharedScreen.java
@@ -10,6 +10,16 @@ package org.bss.brihaspatisync.network.desktop_sharing;
 import java.io.File;
 import java.io.FileInputStream;
 
+import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.Dimension;
@@ -23,6 +33,15 @@ import org.apache.commons.httpclient.methods.PostMethod;
 import org.bss.brihaspatisync.gui.Desktop_Sharing;
 import org.bss.brihaspatisync.util.ClientObject;
 
+import org.bss.brihaspatisync.util.RuntimeDataObject;
+
+import org.apache.commons.httpclient.auth.AuthScope;
+
+import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.HostConfiguration;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+
+
 /**
  * @author <a href="mailto: arvindjss17@gmail.com" > Arvind Pal </a>
  * @author <a href="mailto: ashish.knp@gmail.com" > Ashish Yadav</a>
@@ -35,9 +54,9 @@ public class PostSharedScreen implements Runnable {
 	private boolean flag=false;
 
 	private String reflectorIP ="";
-	
+	private BufferedImage image=null;
 	private ClientObject clientObject=ClientObject.getController();
-
+	private RuntimeDataObject runtime_object=RuntimeDataObject.getController();
 	private static PostSharedScreen post_screen=null;
 
 	/**
@@ -76,6 +95,30 @@ public class PostSharedScreen implements Runnable {
                 }
         }
 
+	public BufferedImage captureScreen() {
+		try{
+			Robot robot = new Robot();
+        	      	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+             		Rectangle size = new Rectangle(0, 0, screenSize.width, screenSize.height);
+                
+			Point mousePosition = MouseInfo.getPointerInfo().getLocation();
+			image = robot.createScreenCapture(size);
+                	Polygon pointer = new Polygon(new int[]{0,16,10,8},new int[]{0,8,10,16},4);
+	                Polygon pointerShadow = new Polygon(new int[]{6,21,16,14},new int[]{1,9,11,17},4);
+        	        Graphics2D gr12D = image.createGraphics();
+                	gr12D.translate(mousePosition.x,mousePosition.y);
+	                gr12D.setColor(new Color(100,100,100,100) );
+        	        gr12D.fillPolygon(pointerShadow);
+                	gr12D.setColor(new Color(100,100,255,200) );
+	                gr12D.fillPolygon(pointer);
+        	        gr12D.setColor(Color.red);
+                	gr12D.drawPolygon(pointer);
+	                gr12D.dispose();
+		}catch(Exception e){System.out.println("Error in capture image"+e.getCause());}
+                return image;
+        }
+
+
 	public void run() {
 		try {
 			Robot robot = new Robot();
@@ -86,10 +129,18 @@ public class PostSharedScreen implements Runnable {
 				HttpClient client = new HttpClient();
 		        	PostMethod postMethod = new PostMethod("http://"+clientObject.getReflectorIP()+":8884");
 				client.setConnectionTimeout(8000);
-               			BufferedImage image = robot.createScreenCapture(size);
-                       		ImageIO.write(image, "jpeg", new File("image.jpeg"));
+                       		ImageIO.write(captureScreen(), "jpeg", new File("image.jpeg"));
                        		postMethod.setRequestBody(new FileInputStream("image.jpeg"));
                			postMethod.setRequestHeader("Content-type","image/jpeg; charset=ISO-8859-1");
+				
+				// Http Proxy Handler
+				if((!(runtime_object.getProxyHost()).equals("")) && (!(runtime_object.getProxyPort()).equals(""))){
+                                        HostConfiguration config = client.getHostConfiguration();
+                                        config.setProxy(runtime_object.getProxyHost(),Integer.parseInt(runtime_object.getProxyPort()));
+                                        Credentials credentials = new UsernamePasswordCredentials(runtime_object.getProxyUser(), runtime_object.getProxyPass());
+                                        AuthScope authScope = new AuthScope(runtime_object.getProxyHost(), Integer.parseInt(runtime_object.getProxyPort()));
+                                        client.getState().setProxyCredentials(authScope, credentials);
+                                }
 
                        		int statusCode1 = client.executeMethod(postMethod);
                        		postMethod.getStatusLine();
