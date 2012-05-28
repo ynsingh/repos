cd569f43f75d5cffde5e6fdfcf05698fedd8c9d9
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2012-05-28 08:06:34 +0000

    YNS: tree conflicts removed from stable build.

--- brihaspati/branches/stable/brihaspati2/SQLscrpts/Modify_Table_InstituteAdminUser18april2012.sql
+++
@@ -1,6 +0,0 @@
-use brihaspati;
-
-alter table INSTITUTE_ADMIN_USER drop column ADMIN_FNAME;
-alter table INSTITUTE_ADMIN_USER drop column ADMIN_LNAME;
-alter table INSTITUTE_ADMIN_USER drop column ADMIN_EMAIL;
-alter table INSTITUTE_ADMIN_USER drop column ADMIN_PASSWORD;
