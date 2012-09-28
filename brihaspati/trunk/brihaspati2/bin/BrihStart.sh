18b0c2ab91bba74649e10ab4c307b34b352f9dba
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2012-09-28 06:21:45 +0000

    YNS: java vm options added for better execution of vm for babylon chat server.

--- brihaspati/trunk/brihaspati2/bin/BrihStart.sh
+++ brihaspati/trunk/brihaspati2/bin/BrihStart.sh
@@ -6,4 +6,4 @@ cd $BRIHASPATI_HOME/tdk-2.3_01/webapps/brihaspati2/babylon
 $BRIHASPATI_HOME/tdk-2.3_01/bin/startup.sh
 CLASSPATH=$CLASSPATH:$BRIHASPATI_HOME/tdk-2.3_01/webapps/brihaspati2/babylon/babylon.jar
 export CLASSPATH
-java babylon.babylonServerStart -nographics -chatlogs -usepasswords & 
+java -Djava.awt.headless=true -Dfile.encoding=UTF-8 -server -XX:-DisableExplicitGC -XX:MaxGCPauseMillis=500 -XX:GCTimeRatio=99 -XX:+UseParallelGC babylon.babylonServerStart -nographics -chatlogs -usepasswords & 
