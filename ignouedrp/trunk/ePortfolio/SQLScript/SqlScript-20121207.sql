2cf90faf0dbd3ea3c5a0f4511cc107b4d97ef02b
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2012-12-07 12:17:20 +0000

    NKS: bug fix.

---
+++ ignouedrp/trunk/ePortfolio/SQLScript/SqlScript-20121207.sql
@@ -0,0 +1,2 @@
+INSERT INTO `grade_type_master` VALUES (1,'Points Grade','','2012-05-12 12:11:50'),(2,'Letter Grade',NULL,'2012-05-12 12:12:50'),(3,'Letter Grade (+/-)',NULL,'2012-05-12 12:13:50');
+INSERT INTO `grade_type_details_master` VALUES (1,1,'Points: '),(2,2,'A: ,B: ,C: ,D: ,F: '),(3,3,'A+: ,A: ,A-: ,B+: ,B: ,B-: ,C+: ,C: ,C-: ,D+: ,D: ,D-: ,F: ');
