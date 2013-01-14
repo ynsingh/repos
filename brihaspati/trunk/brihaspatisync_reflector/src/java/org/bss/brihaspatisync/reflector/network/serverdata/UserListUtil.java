1437136d2d3b3b70bd404830de21fbd876f39232
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2013-01-14 08:29:38 +0000

    Arvind: at session timeout, the clients are closed.

--- brihaspati/trunk/brihaspatisync_reflector/src/java/org/bss/brihaspatisync/reflector/network/serverdata/UserListUtil.java
+++ brihaspati/trunk/brihaspatisync_reflector/src/java/org/bss/brihaspatisync/reflector/network/serverdata/UserListUtil.java
@@ -19,11 +19,12 @@ import org.bss.brihaspatisync.reflector.util.RuntimeDataObject;
 
 public class UserListUtil {
 
-	private String data="";
+	private int count_value=0;
 	private boolean hrflag=false;
 	private boolean presflag=false;
  	private Vector vector=new Vector();
 	private static UserListUtil util=null;
+	private String senduserlist_to_client="";
 	private Vector status_vector=new Vector();	
 	private MyHashTable temp_ht=RuntimeDataObject.getController().getUserListMyHashTable();
 	
@@ -38,88 +39,33 @@ public class UserListUtil {
 	
 	public void addDataForVector(String course_id,String userlistdata) {
                 try {
-			if(!(temp_ht.getStatusCourseId(course_id))) {
-                                temp_ht.setCourseIdUserListVector(course_id,new VectorClass());
-                        }
-			
-			VectorClass vectorclass=temp_ht.getCourseIdUserListVector(course_id);
-			if(!(vectorclass.getValue().equals(userlistdata)))
-				vectorclass.addValue(userlistdata);
-			if(userlistdata.equals("noUser")){
-	                        RuntimeDataObject.getController().resetMastrerReflecterCourseid(course_id);
+			System.out.println("Add user list ---->  "+userlistdata+" course_id "+course_id);	
+			if(userlistdata.equals("noUser")){	
+	                       	RuntimeDataObject.getController().resetMastrerReflecterCourseid(course_id);
 				temp_ht.removeCourseIdUserListVector(course_id);
-				RuntimeDataObject.getController().setHandraiseFlag(false);
-                              	RuntimeDataObject.getController().setHandraiseFlag(false);
-
+				senduserlist_to_client="sessionlist_timeout";
+			} else {
+				if(!(temp_ht.getStatusCourseId(course_id))) {
+                                	temp_ht.setCourseIdUserListVector(course_id,new VectorClass());
+                        	}
+				VectorClass vectorclass=temp_ht.getCourseIdUserListVector(course_id);
+	                        if(!(vectorclass.getValue().equals(userlistdata)))
+        	                        vectorclass.addValue(userlistdata);
 			}
-			strat_stop_HandraiseFlag();
 		} catch(Exception e){ System.out.println("Error in UserListUtil.java line no 50 "+e.getMessage()); }
         }
 	
 	public String getDataForVector(String course_id) {
-		String userlistdata="";
                 try {
 			VectorClass vectorclass=temp_ht.getCourseIdUserListVector(course_id);
 			if(vectorclass!=null){
-	                        userlistdata=vectorclass.getValue();
-				data=userlistdata;
+	                        senduserlist_to_client=vectorclass.getValue();
 			}
-		}catch(Exception e){
-			System.out.println("Error in UserListUtil.java line no 62 "+e.getMessage());
-		}
-		return userlistdata;	
+		}catch(Exception e){ System.out.println("Error in UserListUtil.java line no 62 "+e.getMessage());}
+		
+		String temp_string=senduserlist_to_client;
+		System.out.println("send userlist to client "+senduserlist_to_client);
+		senduserlist_to_client="";
+		return temp_string;	
         }
-	
-	private void strat_stop_HandraiseFlag(){
-		try {
-			String str=data;
-                       	str=str.replaceAll(","," ");
-                        if(str.length()>0){
-                        	java.util.StringTokenizer Tok = new java.util.StringTokenizer(str);
-                                while(Tok.hasMoreElements()) {
-                                	String str1=(String)Tok.nextElement();
-                                        vector.add(str1);
-                           	}
-                      	}
-			for(int i=0;i<vector.size();i++) {
-   				str=vector.get(i).toString();
-                                StringTokenizer st = new StringTokenizer(str, "$");
-                                while(st.hasMoreTokens()) {
-                                	String key = st.nextToken();
-                                        String val = st.nextToken();
-                                        status_vector.add(val);
-                               	}
-					
-                                try {
-                                	if(status_vector.contains("Allow-Mic")) {
-                                                if(!hrflag) {
-                                                	hrflag=true;
-                                                        RuntimeDataObject.getController().setHandraiseFlag(hrflag);
-                                               	}
-                                        }else if(status_vector.contains("Allow-Screen")){
-						if(!presflag) {
-                                                        presflag=true;
-                                                        RuntimeDataObject.getController().setPresentationFlag(presflag);
-                                                }
-					}else{
-                                        	if(hrflag){
-                                                	hrflag=false;
-                                                        RuntimeDataObject.getController().setHandraiseFlag(hrflag);
-                                                }
-						if(presflag){
-                                                        presflag=false;
-                                                        RuntimeDataObject.getController().setHandraiseFlag(presflag);
-                                                }
-                                      	}
-
-                             	} catch(Exception e){}
-                      	}
-                        vector.clear();
-                        status_vector.clear();
-		}catch(Exception w){}	
-	}
-	
-
 }
-
-	
