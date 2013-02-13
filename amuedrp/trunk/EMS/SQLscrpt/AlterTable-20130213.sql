26b499a7d639502efe29c5885bbe193b2f6d834c
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2013-02-13 07:44:57 +0000

    NKS: bug fix.

--- amuedrp/trunk/EMS/SQLscrpt/AlterTable-20130213.sql
+++ amuedrp/trunk/EMS/SQLscrpt/AlterTable-20130213.sql
@@ -1,4 +1,4 @@
-use ems;
+use emsiitk;
 
 CREATE TABLE preferencial_voting (voter_ballot_id VARCHAR(20), position_id VARCHAR(20) NOT NULL, candidate_id VARCHAR(20) NOT NULL, election_id VARCHAR(20) NOT NULL, institute_id VARCHAR(20), voter_id VARCHAR(255) NOT NULL, status VARCHAR(20), preference VARCHAR(20), PRIMARY KEY (candidate_id, election_id, position_id, voter_id));
 
