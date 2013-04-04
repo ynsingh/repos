994d1d55917ddf59a9974cb9ded0b1a1ec7bc175
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2013-04-04 07:47:50 +0000

    Arvind: command line method to start reflector.

---
+++ brihaspati/trunk/brihaspatisync_reflector/reflector_start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+CLASSPATH=bin/classes
+
+for i in lib/*
+ do
+  CLASSPATH=$CLASSPATH:$i
+ done
+export CLASSPATH  
+java -Djavax.net.ssl.trustStore=jnlp/brihaspatisync org.bss.brihaspatisync.reflector.Reflector start &
