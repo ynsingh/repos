7d56fdffaac875b4271d53042757a955b49178e0
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2013-06-07 08:40:08 +0000

    NKS: script to upgrade the database.

---
+++ brihaspati/trunk/BGAS/config/sqlscripts/altertableledgers-20130607.sql
@@ -0,0 +1,14 @@
+alter table ledgers add status int(1) NOT NULL DEFAULT 0 after affects_gross;
+
+CREATE TABLE IF NOT EXISTS budgets (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  code varchar(25) NOT NULL,
+  group_id int(11) NOT NULL,
+  budgetname varchar(100) NOT NULL,
+  bd_balance decimal(15,2) DEFAULT '0.00',
+  op_balance_dc char(1) DEFAULT NULL,
+  type varchar(50) NOT NULL,
+  allowedover int(1) NOT NULL DEFAULT 0,
+  UNIQUE(code,budgetname),
+  PRIMARY KEY (id)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
