3065fbb4a35507085dee08d91d5d775520145fe4
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2013-09-02 12:14:54 +0000

    NKS: minor bug fix in sql script.

--- brihaspati/trunk/BGAS/config/sqlscripts/altertable-settings-20130813.sql
+++ brihaspati/trunk/BGAS/config/sqlscripts/altertable-settings-20130813.sql
@@ -1,3 +1,2 @@
-alter table settings add ins_name varchar(255)default null;
 alter table settings add dept_name varchar(255)default null;
 alter table settings add uni_name varchar(255)default null;
