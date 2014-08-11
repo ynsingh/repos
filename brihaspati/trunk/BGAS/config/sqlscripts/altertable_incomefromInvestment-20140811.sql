1d906cd30fb489ab23f7c7cc2d425bdf83942dcb
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2014-08-11 10:08:07 +0000

    NKS: bug fixe.

---
+++ brihaspati/trunk/BGAS/config/sqlscripts/altertable_incomefromInvestment-20140811.sql
@@ -0,0 +1,1 @@
+alter table income_form_investment add entry_id int(11) NOT NULL after type;
--- brihaspati/trunk/BGAS/system/application/controllers/admin/schema.sql
+++ brihaspati/trunk/BGAS/system/application/controllers/admin/schema.sql
@@ -379,6 +379,7 @@ CREATE TABLE income_from_investment
         amount decimal(15, 2) NOT NULL DEFAULT 0.00,
         date DATETIME NOT NULL,
         type varchar(100) NOT NULL,
+	 entry_id int(11) NOT NULL,
         PRIMARY KEY(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
