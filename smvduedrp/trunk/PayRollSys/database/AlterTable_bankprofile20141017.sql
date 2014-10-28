c6a009c0d2e687e44fc2003aaa78d4d85433a7e6
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2014-10-28 05:38:26 +0000

    Seema: bug fix.

--- smvduedrp/trunk/PayRollSys/database/AlterTable_bankprofile20141017.sql
+++ smvduedrp/trunk/PayRollSys/database/AlterTable_bankprofile20141017.sql
@@ -2,6 +2,6 @@ use pl;
 
 ALTER TABLE bankprofile ADD  account_number bigint(20) NOT NULL after branch_name;
 ALTER TABLE bankprofile ADD  account_type varchar(40) NOT NULL after account_number;
-ALTER TABLE bankprofile ADD  pan_number varchar(10) NOT NULL after type_of_account;
+ALTER TABLE bankprofile ADD  pan_number varchar(10) NOT NULL after account_type;
 ALTER TABLE bankprofile ADD  tan_number varchar(10) NOT NULL after pan_number;
 ALTER TABLE bankprofile ADD  account_name varchar(250) NOT NULL after tan_number;
