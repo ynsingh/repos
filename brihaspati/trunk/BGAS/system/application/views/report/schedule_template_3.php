936364c71bd0356973bcf7351ba825b25d618a1c
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2015-03-02 09:30:04 +0000

    Kanchan: schedule related view updated. Bug fix.

--- brihaspati/trunk/BGAS/system/application/views/report/schedule_template_3.php
+++ brihaspati/trunk/BGAS/system/application/views/report/schedule_template_3.php
@@ -2,53 +2,86 @@
 
 	setlocale(LC_MONETARY, 'en_IN');
 	$this->load->library('balancesheet');
-	
-	$i = 0;
-	if($id != '')
-                $i = $id;
+	$count = 5;
+        $this->load->library('session');
+        $date1 = $this->session->userdata('date1');
+        $date2 = $this->session->userdata('date2');
+        $fy_start=explode("-",$date1);
+        $fy_end=explode("-",$date2);
 
-	$this->db->from('groups');
-	$this->db->select('id, name')->where('parent_id', $i);
-	$group = $this->db->get();
-		
-	foreach($group->result() as $row){
+        $curr_year = '('.$fy_start[0] ."-" .$fy_end[0] .')';
+        $prev_year = '(' . ($fy_start[0]-1) ."-" . ($fy_end[0]-1) .')';
+        $liability = new Balancesheet(); 
 
 		echo "<br><br>";
-		echo "<strong>" . $row->name . "</strong>";
-		echo "<br><br>";
+		echo "<strong>Secured</strong>";
+                echo "<br><br>";
 		echo "<table border=0 class=\"simple-table balance-sheet-table\" >";
 	      	echo "<thead><tr><th></th><th align=\"center\" colspan=\"2\">CURRENT YEAR</th><th align=\"center\" colspan=\"2\">PREVIOUS YEAR</th></tr></thead>";
-		$object = new Balancesheet();
-		$object->init($row->id);
-		$object->schedule_five();
-		$dr_total = $object->dr_total;
-		$cr_total = $object->cr_total;
-		$old_cr_total = $object->old_cr_total;
-		$old_dr_total = $object->old_dr_total;
-
+		
+		$liability->schedule_five('12',5,'100301','view','NULL');
+		$dr_total1 = $liability->dr_total1;
+		$cr_total1 = $liability->cr_total1;
+		$prev_sum = $liability->prev_sum;
+		$prev_sum1 = $liability->prev_sum1;
 		echo "<tr>";
-			echo "<td width=40%>";
-				echo "<b>Total</b>";
-			echo "</td>";
+		echo "<td width=40%>";
+			echo "<b>Total</b>";
+		echo "</td>";
 
-                	echo "<td width=15% align=\"right\">";
-                        	echo  money_format('%!i', convert_cur($dr_total));
-	                echo "</td>";
+                echo "<td width=15% align=\"right\">";
+                echo "<strong>".convert_amount_dc(+$dr_total1)."</strong>";
+	        echo "</td>";
 	
-        	        echo "<td width=15% align=\"right\">";
-                	        echo  money_format('%!i', convert_cur($cr_total));
-	                echo "</td>";
+        	echo "<td width=15% align=\"right\">";
+         	echo  "<strong>".convert_amount_dc(-$cr_total1)."</strong>";
+	        echo "</td>";
 
-        	        echo "<td width=15% align=\"right\">";
-                	        echo  money_format('%!i', convert_cur($old_dr_total));
-	                echo "</td>";
+        	echo "<td width=15% align=\"right\">";
+           	echo "<strong>".convert_amount_dc(+$prev_sum)."</strong>";
+	        echo "</td>";
 
-        	        echo "<td width=15% align=\"right\">";
-                	        echo  money_format('%!i', convert_cur($old_cr_total));
-	                echo "</td>";
+        	echo "<td width=15% align=\"right\">";
+              	echo "<strong>".convert_amount_dc(-$prev_sum1)."</strong>";
+	        echo "</td>";
 		echo "</tr>";
 		echo "</table>";
-		echo "<br><br><br>";
-	}
+		echo "<br><br><br>";  
+		
+		echo "<br><br>";
+                echo "<strong>Unsecured</strong>";
+		echo "<br><br>";
+		echo "<table border=0 class=\"simple-table balance-sheet-table\" >";
+                echo "<thead><tr><th></th><th align=\"center\" colspan=\"2\">CURRENT YEAR</th><th align=\"center\" colspan=\"2\">PREVIOUS YEAR</th></tr></thead>";
+		
+		 $liability->schedule_five('13',5,'100302','view','NULL');
+		 $dr_total1 = $liability->dr_total1;
+                 $cr_total1 = $liability->cr_total1;
+                 $prev_sum = $liability->prev_sum;
+                 $prev_sum1 = $liability->prev_sum1;
 
+	
+		 echo "<tr>";
+                 echo "<td width=40%>";
+                 	echo "<b>Total</b>";
+                 echo "</td>";
+
+                 echo "<td width=15% align=\"right\">";
+                 echo "<strong>".convert_amount_dc(+$dr_total1)."</strong>";
+                 echo "</td>";
+        
+                 echo "<td width=15% align=\"right\">";
+                 echo "<strong>".convert_amount_dc(-$cr_total1)."</strong>";
+                 echo "</td>";
+
+                 echo "<td width=15% align=\"right\">";
+                 echo "<strong>".convert_amount_dc(+$prev_sum)."</strong>";
+                 echo "</td>";
+
+                 echo "<td width=15% align=\"right\">";
+                 echo "<strong>".convert_amount_dc(-$prev_sum1)."</strong>";
+                 echo "</td>";
+                 echo "</tr>";  
+                 echo "</table>";
+                 echo "<br><br><br>";
 ?>
