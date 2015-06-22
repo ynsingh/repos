b7400ce7d5cc2dc34e8246032522fa8d0cb9fd12
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2015-06-22 07:16:26 +0000

    Megha: bug fix in db script

--- brihaspati/trunk/BGAS/config/sqlscripts/createtable_fund_group_table-20150618.sql
+++ brihaspati/trunk/BGAS/config/sqlscripts/createtable_fund_group_table-20150618.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS bgasAccData
+CREATE TABLE IF NOT EXISTS  fund_group_table
 (
     id int(11)  NOT NULL AUTO_INCREMENT,
     code VARCHAR (255) NOT NULL,
     group_id int (11) NOT NULL,
     name VARCHAR (255) NOT NULL,
     short_name VARCHAR (11) NOT NULL,
-    PRIMARY KEY(id),
+    PRIMARY KEY(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
