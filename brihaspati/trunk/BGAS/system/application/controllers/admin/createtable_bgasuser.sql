4fc5dfacc3ee635d06bd9eb7f6a4324f9d2392e9
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2015-10-20 07:25:24 +0000

    Megha: sql file added for authority

--- brihaspati/trunk/BGAS/system/application/controllers/admin/createtable_bgasuser.sql
+++ brihaspati/trunk/BGAS/system/application/controllers/admin/createtable_bgasuser.sql
@@ -131,3 +131,34 @@ CREATE TABLE IF NOT EXISTS forgotPass
 	PRIMARY KEY(ID)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+CREATE TABLE IF NOT EXISTS authorities
+(
+	id int(11) NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	nickname varchar(255) NOT NULL,
+	authority_email varchar(255),
+	PRIMARY KEY(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE IF NOT EXISTS authority_map
+(
+	id int(11) NOT NULL AUTO_INCREMENT,
+	authority_id int(11) NOT NULL,
+	user_id int(11) NOT NULL,
+	map_date DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+	till_date DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+	authority_type varchar(10) NOT NULL, 
+	PRIMARY KEY(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS authority_archive
+(
+	id int(11) NOT NULL AUTO_INCREMENT,
+	authority_id int(11) NOT NULL,
+	user_id int(11) NOT NULL,
+	map_date DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+	till_date DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+	authority_type varchar(10) NOT NULL, 
+	PRIMARY KEY(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
