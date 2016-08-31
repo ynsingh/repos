9fde3f87d0325a6f103190f0d8131644af88e8f6
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2016-08-31 08:20:16 +0000

    NKS: bug fix.

--- brihaspati/trunk/BGAS/system/application/controllers/addparty.php
+++ brihaspati/trunk/BGAS/system/application/controllers/addparty.php
@@ -31,8 +31,8 @@ class Addparty extends Controller {
 		//$this->load->library('session');
         	$asc_order = $this->session->userdata('order_change');
 		$asc_id_order = $this->session->userdata('order_id_change');
-		$this->template->set('nav_links', array('addparty/add' => 'Add Party'));
-		$this->template->set('page_title', 'Add Party ');
+		$this->template->set('nav_links', array('addparty/add' => 'Add Party','addparty/mulparty' => 'Upload Party'));
+		$this->template->set('page_title', 'Show Party List ');
 		if($asc_order == '1'){
 		$this->db->from('addsecondparty')->order_by('partyname', 'asc');
                 $pdetail = $this->db->get();
@@ -165,6 +165,66 @@ class Addparty extends Controller {
 		return;
 	}
 
+//function is used for upload multiple party at a time
+	function mulparty()
+	{
+		$this->load->library('form_validation');
+		$this->template->set('nav_links', array('addparty/add' => 'Add Party','addparty/show' => 'Show Party'));
+		$this->template->set('page_title', 'Upload Party');
+		$logdata['Sr']="";
+		if (isset($_FILES['userfile'])) {
+			if ($_FILES["userfile"]["error"] > 0) {
+        	 		echo "Return Code: " . $_FILES["userfile"]["error"] . "<br />";
+        		}
+			$file_name = $_FILES["userfile"]["name"];
+			$ext = substr(strrchr($file_name, '.'), 1);
+
+	//	if (file_exists($file_name)) {
+			if (file_exists($_FILES["userfile"]["tmp_name"])) {
+				if ($ext == 'txt') {
+				//if (is_readable($file_name)) {
+					if (is_readable($_FILES["userfile"]["tmp_name"])) {
+						$i=1;
+				//	$h = fopen($file_name,"r");
+						$h = fopen($_FILES["userfile"]["tmp_name"],"r");
+				//		$data[] = array();
+						while (false !== ($line = fgets($h)))
+        				        {
+			//				$data[0]="";$data[1]="";$data[2]="";$data[3]="";$data[4]="";$data[5]="";$data[6]="";$data[7]="";$data[8]="";$data[9]="";$data[10]="";$data[11]="";$data[12]="";$data[13]="";$data[14]="";$data[15]="";$data[16]="";$data[17]="";
+	
+						        $data = explode( ";", $line );
+							if(($data[0]=="") || ($data[1]=="")){
+								$msg1=" The Party name or party role is missing";
+							}
+							else{
+								$msg1= $this->addoneparty($data[0],$data[1],$data[2],$data[3],$data[4],$data[5],$data[6],$data[7],$data[8],$data[9],$data[10],$data[11],$data[12],$data[13],$data[14],$data[15],$data[16],$data[17]);
+								//$msg1= addoneparty($data_pname,$data_partyrole,$data_address,$data_accountemail,$data_mnumber,$data_bankname,$data_branchname,$data_bankaddress,$data_bacnumber,$data_ifsccode,$data_pannum,$data_uidnum,$data_tannum,$data_stnm,$data_vatnum,$data_gstnum,$data_op_balance_dc,$data_opbal);
+							}
+			//				print_r($msg1);
+				//			print_r ($data);
+							$logdata['Sr'][$i]=$i.": ".$msg1;
+		        				$i++;
+							
+	        	        		}
+				//		print_r($_FILES["userfile"]["tmp_name"]);
+			 			$this->messages->add(" File has been uploaded successfully. ", 'success');
+						fclose($h);
+					}
+					else {
+				 		$this->messages->add(" File does not readable. ", 'error');
+					}
+				}
+				else {
+					$this->messages->add(" File not having txt extension. ", 'error');
+				}
+			}
+			else {
+				$this->messages->add($file_name." File does not exist. ", 'error');
+			}
+		}
+		$this->template->load('template', 'addparty/mulparty', $logdata);
+                return ;
+	}
 
 	function add()
 	{
@@ -388,7 +448,10 @@ class Addparty extends Controller {
 			$data_op_balance_dc = $this->input->post('op_balance_dc', TRUE);
 			$data_partyrole = $this->input->post('partyrole', TRUE);
 
+			$msg1= $this->addoneparty($data_pname,$data_partyrole,$data_address,$data_accountemail,$data_mnumber,$data_bankname,$data_branchname,$data_bankaddress,$data_bacnumber,$data_ifsccode,$data_pannum,$data_uidnum,$data_tannum,$data_stnm,$data_vatnum,$data_gstnum,$data_op_balance_dc,$data_opbal);
+
 //add in new method
+/*
                         if($data_partyrole == "faculty"){
                         	$secunit_id = "00";
                         }
@@ -512,14 +575,14 @@ class Addparty extends Controller {
 				$tan=$row->tan;
 				$stnumber=$row->staxnum;
 				$vat=$row->vat;
-				$gst=$row->gst;
+				$gst=$row->gst;*/
 		/*		if($sacunit == $data_sacunitid)
 				{
                         		$this->messages->add(' Party with sunitid ' .$data_sacunitid. ' already exist. ', 'error');
                 			$this->template->load('template', 'addparty/add', $data);
                         		return;
 				}*/
-				if($data_accountemail !=""){
+		/*		if($data_accountemail !=""){
 				if($emailid == $data_accountemail)
 				{
                         		$this->messages->add(' Party with email ' .$data_accountemail. ' already exist. ', 'error');
@@ -584,21 +647,35 @@ class Addparty extends Controller {
 				}
 				}
                 	}
- 			//For Create Folder Automatically Under The BGAS Folder.  
-       			$this->upload_path= realpath(BASEPATH.'../');
-        		if (!is_dir($this->upload_path . "/uploads/scopy")) {
-                		$result = mkdir($this->upload_path . "/uploads/scopy",0777,true);
-        		}//end
-			$this->upload_path= realpath(BASEPATH.'../uploads/scopy');
+*/
+			if(strpos($msg1,'successfully') !== false){
+ 				//For Create Folder Automatically Under The BGAS Folder.  
+	       			$this->upload_path= realpath(BASEPATH.'../');
+        			if (!is_dir($this->upload_path . "/uploads/scopy")) {
+                			$result = mkdir($this->upload_path . "/uploads/scopy",0777,true);
+        			}//end
+				$this->upload_path= realpath(BASEPATH.'../uploads/scopy');
 			
-			if (($_FILES['panfile']['name']!='')&&($data_pannum !='')){
-				$this->upload_model->scancopy_fileupload('panfile',$data_pannum,$secondary_id);
-			}
-			if (($_FILES['uidfile']['name']!='')&&($data_uidnum !=''))
-				$this->upload_model->scancopy_fileupload('uidfile',$data_uidnum,$secondary_id);
-			if (($_FILES['bankfile']['name']!='')&&($data_bacnumber !=''))
-				$this->upload_model->scancopy_fileupload('bankfile',$data_bacnumber,$secondary_id);
+				if (($_FILES['panfile']['name']!='')&&($data_pannum !='')){
+					$this->upload_model->scancopy_fileupload('panfile',$data_pannum,$secondary_id);
+				}
+				if (($_FILES['uidfile']['name']!='')&&($data_uidnum !=''))
+					$this->upload_model->scancopy_fileupload('uidfile',$data_uidnum,$secondary_id);
+				if (($_FILES['bankfile']['name']!='')&&($data_bacnumber !=''))
+					$this->upload_model->scancopy_fileupload('bankfile',$data_bacnumber,$secondary_id);
 
+				$this->messages->add($msg1,'success');
+				redirect('addparty/add');
+                                return;
+			}
+			else{
+				$this->messages->add($msg1,'error');
+				$this->template->load('template', 'addparty/add', $data);
+				return;
+	
+
+			}
+/*
 			$this->db->trans_start();
 			$insert_data=array(
 				'sacunit' =>$secondary_id,
@@ -641,10 +718,238 @@ class Addparty extends Controller {
                 			redirect('addparty/add');
                 			return;
                         }
-
+*/
 		}
+
                 $this->template->load('template', 'addparty/add', $data);
                 return;
+
+	}
+
+
+	function addoneparty($data_pname,$data_partyrole,$data_address,$data_accountemail,$data_mnumber,$data_bankname,$data_branchname,$data_bankaddress,$data_bacnumber,$data_ifsccode,$data_pannum,$data_uidnum,$data_tannum,$data_stnm,$data_vatnum,$data_gstnum,$data_op_balance_dc,$data_opbal){
+	
+			$secunit_id="";
+			if($data_partyrole == "faculty"){
+                                $secunit_id = "00";
+                        }
+                        if($data_partyrole == "student"){
+                                $secunit_id = "01";
+                        }
+                        if($data_partyrole == "staff clerical"){
+                                $secunit_id = "02";
+                        }
+                        if($data_partyrole == "technical staff"){
+                                $secunit_id = "03";
+                        }
+                        if($data_partyrole == "supplier"){
+                                $secunit_id = "04";
+                        }
+                        if($data_partyrole == "admin staff"){
+                                $secunit_id = "05";
+                        }
+                        if($data_partyrole == "contractor"){
+                                $secunit_id = "06";
+                        }
+                        if($data_partyrole == "service provider"){
+                                $secunit_id = "07";
+                        }
+                        if($data_partyrole == "alumni/doner"){
+                                $secunit_id = "08";
+                        }
+                        if($data_partyrole == "author") {
+                                $secunit_id = '09';
+                        }
+                        if($data_partyrole == 'illustrator') {
+                                $secunit_id = '10';
+                        }
+                        if($data_partyrole == 'participant') {
+                                $secunit_id = '11';
+                        }
+                        if($data_partyrole == 'invites/Speaker') {
+                                $secunit_id = '12';
+                        }
+                        if($data_partyrole == 'officers') {
+                                $secunit_id = '13';
+                        }
+			if($secunit_id == ""){
+				$msg1= 'The party role is invalid.';
+				return $msg1;
+			}
+				
+
+                        $number = $this->upload_model->get_count_num($data_partyrole);
+                        $secondary_id = $this->upload_model->get_random_secunitid($secunit_id,$number);
+
+			if($data_mnumber !=""){
+                        if(strlen($data_mnumber) < 10){
+                                $msg1= 'Mobile Number should be 10 or 13 digits.';
+				return $msg1;
+                        }
+                        }
+                        if($data_bacnumber !=""){
+                        if((strlen($data_bacnumber) < 13)|| (strlen($data_bacnumber) > 20)){
+                                $msg1= 'Bank A/C number should be between 13 and 20 digits.';
+				return $msg1;
+                        }
+                        }
+
+                        if($data_ifsccode !=""){
+                        if(strlen($data_ifsccode) < 11){
+                                $msg1= 'IFSC code should be 11 digits.';
+				return $msg1;
+                        }
+                        }
+                        if($data_pannum !=""){
+                        if(strlen($data_pannum) < 10){
+                                $msg1= 'PAN should be 10 digits.';
+				return $msg1;
+                        }
+                        }
+                        if($data_uidnum != ""){
+                        if(strlen($data_uidnum) < 12){
+                                $msg1= 'UID should be 12 digits.';
+				return $msg1;
+                        }
+                        }
+                        if($data_tannum !=""){
+                        if(strlen($data_tannum) < 10){
+                                $msg1= 'TAN should be 10 to 15 digits.';
+				return $msg1;
+                        }
+                        }
+			if($data_stnm !=""){
+                        if(strlen($data_stnm) < 15){
+                                $msg1= 'Service tax number should be 15 digits.';
+				return $msg1;
+                        }
+                        }
+                        if($data_vatnum !=""){
+                        if(strlen($data_vatnum) < 20){
+                                $msg1= 'VAT number should be 20 digits.';
+				return $msg1;
+                        }
+                        }
+                        if($data_gstnum !=""){
+                        if(strlen($data_gstnum) < 20){
+                                $msg1= 'GST number should be 20 digits.';
+				return $msg1;
+                        }
+                        }
+                        $this->db->from('addsecondparty');
+                        $pdetail = $this->db->get();
+                        foreach ($pdetail->result() as $row)
+                        {
+                                //$sacunit=$row->sacunit;
+                                $emailid=$row->email;
+                                $acnum=$row->bancacnum;
+                                $pan=$row->pan;
+                                $uid=$row->u_id;
+                                $tan=$row->tan;
+                                $stnumber=$row->staxnum;
+                                $vat=$row->vat;
+                                $gst=$row->gst;
+				if($data_accountemail !=""){
+                                if($emailid == $data_accountemail)
+                                {
+                                        $msg1= ' Party with email ' .$data_accountemail. ' already exist. ';
+					return $msg1;
+                                }
+                                }
+                                if($data_bacnumber !="" ){
+                                if($acnum == $data_bacnumber)
+                                {
+                                        $msg1= ' Party with A/C ' .$data_bacnumber. ' already exist. ';
+					return $msg1;
+                                }
+                                }
+                                if($data_pannum !=""){
+                                if($pan == $data_pannum)
+                                {
+                                        $msg1= ' Party with Pan Number ' .$data_pannum. ' already exist. ';
+					return $msg1;
+                                }
+                                }
+                                if($data_uidnum !=""){
+                                if($uid == $data_uidnum)
+                                {
+                                        $msg1= ' Party with UID Number ' .$data_uidnum. ' already exist. ';
+					return $msg1;
+                                }
+                                }
+                                if($data_tannum !=""){
+                                if($tan == $data_tannum)
+                                {
+                                        $msg1= ' Party with Tan Number ' .$data_tannum. ' already exist. ';
+					return $msg1;
+                                }
+                                }
+                                if($data_stnm !=""){
+                                if($stnumber == $data_stnm)
+                                {
+                                        $msg1= ' Party with Service Tax Number ' .$data_stnm. ' already exist. ';
+					return $msg1;
+                                }
+                                }
+				if($data_vatnum !=""){
+                                if($vat == $data_vatnum)
+                                {
+                                        $msg1= ' Party with Vat Number ' .$data_vatnum. ' already exist. ';
+					return $msg1;
+                                }
+                                }
+                                if($data_gstnum !=""){
+                                if($gst == $data_gstnum)
+                                {
+                                       $msg1= ' Party with GST Number ' .$data_gstnum. ' already exist. ';
+				       return $msg1;
+                                }
+                                }
+                        }
+			if($data_op_balance_dc == ""){
+				$data_op_balance_dc='D';
+			}
+			
+
+			$this->db->trans_start();
+                        $insert_data=array(
+                                'sacunit' =>$secondary_id,
+                                'partyname' =>$data_pname,
+                                'mobnum' =>$data_mnumber,
+                                'email' =>$data_accountemail,
+                                'address' =>$data_address,
+                                'permanentaddress' =>$data_address,
+                                'bancacnum' =>$data_bacnumber,
+                                'bankname' =>$data_bankname,
+                                'branchname' =>$data_branchname,
+                                'ifsccode' =>$data_ifsccode,
+                                'bankaddress' =>$data_bankaddress,
+                                'pan' =>$data_pannum,
+                                'u_id' =>$data_uidnum,
+                                'tan' =>$data_tannum,
+                                'staxnum' =>$data_stnm,
+                                'partyrole' =>$data_partyrole,
+                                'vat' =>$data_vatnum,
+                                'gst' =>$data_gstnum,
+                                'opbal' =>$data_opbal,
+                                'dc'=>$data_op_balance_dc,
+                        );
+
+                        $sunitid = $insert_data['sacunit'];
+
+                        if ( ! $this->db->insert('addsecondparty', $insert_data))
+                        {
+                                $this->db->trans_rollback();
+                                $this->logger->write_message("error", "Error adding second party");
+                                $msg1= 'Error addding Secondary Unit.';
+				return $msg1;
+                        }
+                        else {
+                                $this->db->trans_complete();
+                                $this->logger->write_message("success"," Party " . $data_pname . " added successfully. ");
+                                $msg1= ' Party Name ' . $data_pname . ' added successfully. ';
+				return $msg1;
+                        }
 	}
 	function edit($sunitid)
 	{
@@ -945,12 +1250,14 @@ class Addparty extends Controller {
 			if ( ! $this->db->where('sacunit', $data_sunitid)->update('addsecondparty', $update_data))
 			{
 				$this->db->trans_rollback();
+				$this->logger->write_message("error", "Error updating Secondary Unit ".$data_sunitid);
 				$this->messages->add('Error updating Secondary Unit.', 'error');
 				$this->template->load('template', 'addparty/edit', $data);
 				return;
 			}
 			else{
                         	$this->db->trans_complete();
+				 $this->logger->write_message("success"," Party " . $data_sunitid . " updated successfully. ");
                                 $this->messages->add('Updated Secondary Unit Id ' . $data_sunitid . '.', 'success');
 				redirect('addparty/show');
 				return;
