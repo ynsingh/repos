d7ac771994bc8540e454e9ab8dfc7639b311f681
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2017-04-20 07:17:12 +0000

    NKSingh: menu stubs added.

--- brihaspati/trunk/SLCMS/application/views/home.php
+++ brihaspati/trunk/SLCMS/application/views/home.php
@@ -4,12 +4,18 @@
         <title>Welcome to IGNTU</title>
     </head>
     <body>
+
+<div >
+<div id="body">
 	<?php $this->load->view('template/header'); ?>
      <h1>Welcome <?= $this->session->userdata('username') ?> <a href="<?= site_url('home/logout') ?>">Logout</a> </h1>
    <!--   <a href="<?= site_url('home/logout') ?>">Logout</a>-->
+	
 	<?php $this->load->view('template/menu'); ?>
-
-
+	</div>
+	
 	<?php $this->load->view('template/footer'); ?>
+	
+</div>
     </body>
 </html>
--- brihaspati/trunk/SLCMS/application/views/template/menu.php
+++ brihaspati/trunk/SLCMS/application/views/template/menu.php
@@ -2,19 +2,82 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
  
-
+echo "<div id=\"header\">";
+echo "<nav>";
 echo "<ul class=\"sf-menu\">";
 			echo "<li class=\"current\">";
-				//echo "<a href=" . base_url() . "title=\"Dashboard\">Dashboard</a>";
+				echo "<a href=" . base_url() . "title=\"Dashboard\">Dashboard</a>";
+			echo "</li>";
+			echo "<li>";
 				echo anchor('setup', 'Setup', array('title' => 'Setup'));
+				echo "<ul>";
+					echo "<li>";
+						echo anchor('add/emailsetting', 'Email Setting', array('title' => 'Email Setting'));
+					echo "</li>";
+					echo "<li>";
+						echo anchor('add/role', 'Add Role', array('title' => 'Add Role'));
+					echo "</li>";
+					echo "<li>";
+						echo anchor('add/seatreserve', 'Add Seat Reservation', array('title' => 'Add Seat Reservation'));
+					echo "</li>";
+					echo "<li>";
+						echo anchor('add/sc', 'Add Study center', array('title' => 'Add Study center'));
+					echo "</li>";
+					echo "<li>";
+						echo anchor('add/dept', 'Add Department', array('title' => 'Add Department'));
+					echo "</li>";				
+					echo "<li>";
+						echo anchor('add/program', 'Add Program', array('title' => 'Add Program'));
+					echo "</li>";
+					echo "<li>";
+						echo anchor('add/fees', 'Add Program Fees', array('title' => 'Add Program Fees'));
+					echo "</li>";
+					
+				echo "</ul>";
 			echo "</li>";
 			echo "<li>";
 				echo anchor('map', 'Map', array('title' => 'Map'));
+				echo "<ul>";
+					echo "<li>";
+						echo anchor('map/userroletype', 'Map User with Role', array('title' => 'Map User with Role'));
+					echo "</li>";
+					echo "<li>";
+						echo anchor('map/programsubject', 'Map Program with Subject', array('title' => 'Map Program with Subject'));
+					echo "</li>";
+					echo "<li>";
+						echo anchor('map/subjectpaper', 'Map Subject with Paper', array('title' => 'Map Subject with Paper'));
+					echo "</li>";
+					echo "<li>";
+						echo anchor('map/subjectteacher', 'Map Subject and Paper with Teacher', array('title' => 'Map Subject and Paper with Teacher'));
+					echo "</li>";
+					
+					
+				echo "</ul>";
 			echo "</li>";
 			echo "<li>";
 				echo anchor('upload', 'Upload', array('title' => 'Upload')); 
+				echo "<ul>";
+					echo "<li>";
+						echo anchor('upl/uploadlogo', 'Upload Logo', array('title' => 'Upload Logo'));
+					echo "</li>";
+					echo "<li>";
+						echo anchor('upl/uploadstulist', 'Upload Student List', array('title' => 'Upload Student List'));
+					echo "</li>";
+					echo "<li>";
+						echo anchor('upl/uploadteachlist', 'Upload Teacher List', array('title' => 'Upload Teacher List'));
+					echo "</li>";
+					
+					
+					
+				echo "</ul>";
 			echo "</li>";
 			echo "<li>";
 				echo anchor('report', 'Report', array('title' => 'Report')); 
 			echo "</li>";
+			echo "<li>";
+				echo anchor('profile', 'Profile', array('title' => 'Profile')); 
+			echo "</li>";
 echo "</ul>";
+echo "</nav>";
+echo "</div>";
+
