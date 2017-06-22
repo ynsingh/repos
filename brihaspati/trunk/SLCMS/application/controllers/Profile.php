7e03f5654a1fa3b387841db79e7075a54cc7a288
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2017-06-22 08:10:00 +0000

    Sharad: bug fix.

--- brihaspati/trunk/SLCMS/application/controllers/Profile.php
+++ brihaspati/trunk/SLCMS/application/controllers/Profile.php
@@ -3,6 +3,7 @@
 /* 
  * @name Profile.php
  * @author Deepika Chaudhary (chaudharydeepika88@gmail.com)
+ * @author Sharad Singh (sharad23nov@gmail.com) modify view profile
  */
 
 defined('BASEPATH') OR exit('No direct script access allowed');
@@ -78,16 +79,17 @@ public function changepasswd()
 
 
 public function viewprofile(){
-/* get logged user detail from different tables (firstname, lastname, email, address, mobile number, secondary email, campus name, org name)
+        /* get logged user detail from different tables (firstname, lastname, email, address, mobile number, secondary email, campus name, org name)
          * using login model and common model
          */
-	$this->currentlog=$this->session->userdata('username');
-        $this->roleid=$this->common_model->get_listspfic1('user_role_type','roleid','userid');
-        $this->currentrole=$this->common_model->get_listspfic1('role','role_name','role_id');
+	    $this->currentlog=$this->session->userdata('username');
+        //$this->roleid=$this->common_model->get_listspfic1('user_role_type','roleid','userid');
+        $this->roleid=$this->session->userdata('id_role');
+        $this->currentrole=$this->common_model->get_listspfic1('role','role_name','role_id',$this->roleid);
         $this->name=$this->login_model->get_listspfic1('userprofile','firstname','userid');
         $this->lastn=$this->login_model->get_listspfic1('userprofile','lastname','userid');
         $this->address=$this->login_model->get_listspfic1('userprofile','address','userid');
-	$this->secmail=$this->login_model->get_listspfic1('userprofile','secmail','userid');
+	    $this->secmail=$this->login_model->get_listspfic1('userprofile','secmail','userid');
         $this->mobile=$this->login_model->get_listspfic1('userprofile','mobile','userid');
         $this->email=$this->login_model->get_listspfic1('edrpuser','email','id');
         $this->campusid=$this->common_model->get_listspfic1('user_role_type','scid','userid');
