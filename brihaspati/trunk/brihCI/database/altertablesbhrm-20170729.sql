467b55624f54fd729df5cf9da1ef010149c42061
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2017-07-31 06:50:14 +0000

    NKS: database schema file added for hrms

--- brihaspati/trunk/brihCI/database/altertablesbhrm-20170729.sql
+++ brihaspati/trunk/brihCI/database/altertablesbhrm-20170729.sql
@@ -1,45 +1,6 @@
-
--- --------------------------------------------------------
-
-DROP DATABASE IF EXISTS `bhrm`;
-CREATE DATABASE `bhrm`;
-USE `bhrm`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `applicant_api`
---
-
-DROP TABLE IF EXISTS `applicant_api`;
-
-CREATE TABLE `applicant_api` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `applicant_id` int(11) DEFAULT NULL,
-  `app_category` varchar(255) DEFAULT NULL,
-  `app_catname` varchar(255) DEFAULT NULL,
-  `app_subcategory` varchar(255) DEFAULT NULL,
-  `app_activities` varchar(255) DEFAULT NULL,
-  `app_maximumscore` varchar(255) DEFAULT NULL,
-  `app_noofarticle` varchar(255) DEFAULT NULL,
-  `app_scoreclaimed` varchar(255) DEFAULT NULL,
-  `app_scoreverified` varchar(255) DEFAULT NULL,
-  `app_varifiername` varchar(255) DEFAULT NULL,
-  `app_varifydate` varchar(255) DEFAULT NULL,
-  `app_remark` varchar(255) DEFAULT NULL,
-  `app_ext1` varchar(200) DEFAULT NULL,
-  `app_ext2` varchar(200) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `applicant_data`
---
+use bhrm; 
 
 DROP TABLE IF EXISTS `applicant_data`;
-
 CREATE TABLE `applicant_data` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `apd_no` varchar(11) DEFAULT NULL,
@@ -77,90 +38,24 @@ CREATE TABLE `applicant_data` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
--- --------------------------------------------------------
-
---
--- Table structure for table `applicant_education`
---
-
-CREATE TABLE `applicant_education` (
-  `aedu_id` int(11) NOT NULL,
-  `aedu_amid` int(11) NOT NULL,
-  `aedu_class` varchar(255) NOT NULL,
-  `aedu_institution` varchar(255) NOT NULL,
-  `aedu_board` varchar(255) NOT NULL,
-  `aedu_subject` varchar(255) NOT NULL,
-  `aedu_passingyear` varchar(255) NOT NULL,
-  `aedu_resultstatus` varchar(255) NOT NULL,
-  `aedu_maxmarks` varchar(255) NOT NULL,
-  `aedu_marksobtained` varchar(255) NOT NULL,
-  `aedu_percentage` varchar(255) NOT NULL,
-  `aedu_ext1` varchar(255) DEFAULT NULL,
-  `aedu_ext2` varchar(255) DEFAULT NULL
+CREATE TABLE `applicant_api` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `applicant_id` int(11) DEFAULT NULL,
+  `app_category` varchar(255) DEFAULT NULL,
+  `app_catname` varchar(255) DEFAULT NULL,
+  `app_subcategory` varchar(255) DEFAULT NULL,
+  `app_activities` varchar(255) DEFAULT NULL,
+  `app_maximumscore` varchar(255) DEFAULT NULL,
+  `app_noofarticle` varchar(255) DEFAULT NULL,
+  `app_scoreclaimed` varchar(255) DEFAULT NULL,
+  `app_scoreverified` varchar(255) DEFAULT NULL,
+  `app_varifiername` varchar(255) DEFAULT NULL,
+  `app_varifydate` varchar(255) DEFAULT NULL,
+  `app_remark` varchar(255) DEFAULT NULL,
+  `app_ext1` varchar(200) DEFAULT NULL,
+  `app_ext2` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-ALTER TABLE `applicant_education`
-  ADD PRIMARY KEY (`aedu_id`);
-ALTER TABLE `applicant_education`
-  MODIFY `aedu_id` int(11) NOT NULL AUTO_INCREMENT;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `applicant_employment`
---
-
-CREATE TABLE `applicant_employment` (
-  `aemp_id` int(11) NOT NULL,
-  `aemp_amid` int(11) NOT NULL,
-  `aemp_officename` varchar(255) NOT NULL,
-  `aemp_post` varchar(255) NOT NULL,
-  `aemp_pay` varchar(255) NOT NULL,
-  `aemp_grade` varchar(255) NOT NULL,
-  `aemp_appoinmentnature` varchar(255) NOT NULL,
-  `aemp_doj` varchar(255) NOT NULL,
-  `aemp_dol` varchar(255) NOT NULL,
-  `aemp_remarks` varchar(255) NOT NULL,
-  `aemp_experience` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-ALTER TABLE `applicant_employment`
-  ADD PRIMARY KEY (`aemp_id`);
-ALTER TABLE `applicant_employment`
-  MODIFY `aemp_id` int(11) NOT NULL AUTO_INCREMENT;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `applicant_entrance_exam`
---
-
-CREATE TABLE `applicant_entrance_exam` (
-  `aeex_id` int(11) NOT NULL,
-  `aeex_smid` int(11) NOT NULL,
-  `aeex_examname` varchar(255) NOT NULL,
-  `aeex_rollno` varchar(255) NOT NULL,
-  `aeex_rank` int(11) NOT NULL,
-  `aeex_score` int(11) NOT NULL,
-  `aeex_state` varchar(255) NOT NULL,
-  `aeex_subject` varchar(255) NOT NULL,
-  `aeex_passingyear` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-ALTER TABLE `applicant_entrance_exam`
-  ADD PRIMARY KEY (`aeex_id`);
-ALTER TABLE `applicant_entrance_exam`
-  MODIFY `aeex_id` int(11) NOT NULL AUTO_INCREMENT;
-
--- ----------------------------------------------------------
-
---
--- Table structure for table `applicant_performance`
---
-
-
-DROP TABLE IF EXISTS `applicant_performance`;
 
 CREATE TABLE `applicant_performance` (
   `aperfor_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -174,14 +69,6 @@ CREATE TABLE `applicant_performance` (
   `aperfor_ext2` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`aperfor_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `applicant_research`
---
-
-DROP TABLE IF EXISTS `applicant_research`;
 
 CREATE TABLE `applicant_research` (
   `ares_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -200,14 +87,6 @@ CREATE TABLE `applicant_research` (
   PRIMARY KEY (`ares_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `applicant_research_degree`
---
-
-DROP TABLE IF EXISTS `applicant_research_degree`;
-
 CREATE TABLE `applicant_research_degree` (
   `aresdeg_id` int(11) NOT NULL AUTO_INCREMENT,
   `aresdeg_amid` int(11) DEFAULT NULL,
@@ -222,14 +101,6 @@ CREATE TABLE `applicant_research_degree` (
   `aresdeg_ext2` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`aresdeg_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `applicant_supportdata`
---
-
-DROP TABLE IF EXISTS `applicant_supportdata`;
 
 CREATE TABLE `applicant_supportdata` (
   `asuppd_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -251,14 +122,6 @@ CREATE TABLE `applicant_supportdata` (
   PRIMARY KEY (`asuppd_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `applicant_total_api`
---
-
-DROP TABLE IF EXISTS `applicant_total_api`;
-
 CREATE TABLE `applicant_total_api` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ata_applicantid` int(11) DEFAULT NULL,
@@ -270,14 +133,6 @@ CREATE TABLE `applicant_total_api` (
   `ata_remark` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `applicant_uploaddata`
---
-
-DROP TABLE IF EXISTS `applicant_uploaddata`;
 
 CREATE TABLE `applicant_uploaddata` (
   `aupd_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -304,38 +159,6 @@ CREATE TABLE `applicant_uploaddata` (
   PRIMARY KEY (`aupd_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `ApplicationStep`
---
-
-CREATE TABLE `applicationstep` ( 
-	`id` INT(11) NOT NULL AUTO_INCREMENT , 
-	`application_no` VARCHAR(255) NOT NULL , 
-	`applicant_masterid` VARCHAR(255) NOT NULL , 
-	`step1_status` VARCHAR(255) NOT NULL , 
-	`step1_date` DATETIME DEFAULT CURRENT_TIMESTAMP ,
-	`step2_status` VARCHAR(255) NOT NULL , 
-	`step2_date` DATETIME DEFAULT CURRENT_TIMESTAMP ,
-	`step3_status` VARCHAR(255) NOT NULL , 
-	`step3_date` DATETIME DEFAULT CURRENT_TIMESTAMP ,
-	`step4_status` VARCHAR(255) NOT NULL , 
-	`step4_date` DATETIME DEFAULT CURRENT_TIMESTAMP ,
-	`step5_status` VARCHAR(255) NOT NULL , 
-	`step5_date` DATETIME DEFAULT CURRENT_TIMESTAMP , 
-	`ext1` VARCHAR(255) NULL , 
-	PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `form_shortname`
---
-
-DROP TABLE IF EXISTS `form_shortname`;
-
 CREATE TABLE `form_shortname` (
   `form_id` int(11) NOT NULL AUTO_INCREMENT,
   `form_advno` varchar(255) DEFAULT NULL,
@@ -345,14 +168,6 @@ CREATE TABLE `form_shortname` (
   `form_posttype` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`form_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `job_open`
---
-
-DROP TABLE IF EXISTS `job_open`;
 
 CREATE TABLE `job_open` (
   `job_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -381,38 +196,4 @@ CREATE TABLE `job_open` (
   PRIMARY KEY (`job_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
---
--- Table structure for table `logs`
---
 
-CREATE TABLE IF NOT EXISTS logs (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  date datetime NOT NULL,
-  level int(1) NOT NULL,
-  host_ip varchar(25) NOT NULL,
-  user varchar(25) NOT NULL,
-  url varchar(255) NOT NULL,
-  user_agent varchar(100) NOT NULL,
-  message_title varchar(255) NOT NULL,
-  message_desc mediumtext NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_role_type`
---
-
-CREATE TABLE IF NOT EXISTS `user_role_type` (
-  `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
-  `userid` int(11) NOT NULL,
-  `roleid` int(11) NOT NULL,
-  `scid` int(10) NOT NULL,
-  `deptid` int(10) DEFAULT NULL,
-  `usertype` varchar(255) NOT NULL,
-  `ext1` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-insert into user_role_type values (1,1,1,1,1,'Administrator','');
