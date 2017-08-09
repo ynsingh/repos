1d71ba03a25deead482616f569ee04f943bf34b7
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2017-08-09 09:35:35 +0000

    NKS: bug fix

--- brihaspati/trunk/brihCI/database/altertableolas-20170809.sql
+++ brihaspati/trunk/brihCI/database/altertableolas-20170809.sql
@@ -1,2 +1,2 @@
 drop index `prg_category` on `program`;
-create index `prg_category`(`prg_cid`,`prg_category`,`prg_name`,`prg_branch`) on `program`;
+create index `prg_category`on `program` (`prg_scid`,`prg_category`,`prg_name`,`prg_branch`);
