d1dd1e0315e87e1d823957e523ac17a3b6540643
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2017-10-03 10:13:53 +0000

    NKS: achema addd ( pre admission step and upload data) and menu updated regarding pre admission exam

--- brihaspati/trunk/brihCI/database/olas.sql
+++ brihaspati/trunk/brihCI/database/olas.sql
@@ -51,6 +51,7 @@ CREATE TABLE `admissionstudent_master` (
   `asm_applicationno` varchar(255) DEFAULT NULL,
   `asm_meritno` varchar(255) DEFAULT NULL,
   `asm_testname` varchar(255) DEFAULT NULL,
+  `asm_coursename` VARCHAR(255) NULL,
   `asm_universitycode` varchar(255) DEFAULT NULL,
   `asm_enterenceexamcenter` VARCHAR(255) NULL,
   `asm_sccode` varchar(255) DEFAULT NULL,
@@ -210,6 +211,7 @@ ALTER TABLE `admissionstudent_registration`
 
 CREATE TABLE `enterence_exam_center` (
   `eec_id` int(11) NOT NULL,
+  `eec_code` varchar(255) DEFAULT NULL,
   `eec_name` varchar(255) DEFAULT NULL,
   `eec_address` blob,
   `eec_city` varchar(255) DEFAULT NULL,
@@ -227,6 +229,58 @@ ALTER TABLE `enterence_exam_center`
 ALTER TABLE `enterence_exam_center`
   MODIFY `eec_id` int(11) NOT NULL AUTO_INCREMENT;
 
+CREATE TABLE `admissionstudent_uploaddata` (
+  `asupd_id` int(11) NOT NULL,
+  `asupd_smid` int(11) NOT NULL,
+  `asupd_enclosure1` varchar(255) DEFAULT NULL,
+  `asupd_enclosure2` varchar(255) DEFAULT NULL,
+  `asupd_enclosure3` varchar(255) DEFAULT NULL,
+  `asupd_enclosure4` varchar(255) DEFAULT NULL,
+  `asupd_enclosure5` varchar(255) DEFAULT NULL,
+  `asupd_enclosure6` varchar(255) DEFAULT NULL,
+  `asupd_enclosure7` varchar(255) DEFAULT NULL,
+  `asupd_enclosure8` varchar(255) DEFAULT NULL,
+  `asupd_enclosure9` varchar(255) DEFAULT NULL,
+  `asupd_enclosure10` varchar(255) DEFAULT NULL,
+  `asupd_enclosure11` varchar(255) DEFAULT NULL,
+  `asupd_enclosure12` varchar(255) DEFAULT NULL,
+  `asupd_enclosure13` varchar(255) DEFAULT NULL,
+  `asupd_enclosure14` varchar(255) DEFAULT NULL,
+  `asupd_enclosure15` varchar(255) DEFAULT NULL,
+  `asupd_photo` varchar(500) DEFAULT NULL,
+  `asupd_signature` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+ALTER TABLE `admissionstudent_uploaddata`
+  ADD PRIMARY KEY (`asupd_id`);
+
+ALTER TABLE `admissionstudent_uploaddata`
+  MODIFY `asupd_id` int(11) NOT NULL AUTO_INCREMENT;
+
+CREATE TABLE `admissionstudent_enterencestep` (
+  `id` int(11) NOT NULL,
+  `application_no` varchar(255) NOT NULL,
+  `admission_masterid` varchar(255) NOT NULL,
+  `step1_status` varchar(255) NOT NULL,
+  `step1_date` datetime DEFAULT NULL,
+  `step2_status` varchar(255) NOT NULL,
+  `step2_date` datetime DEFAULT NULL,
+  `step3_status` varchar(255) NOT NULL,
+  `step3_date` datetime DEFAULT NULL,
+  `step4_status` varchar(255) NOT NULL,
+  `step4_date` datetime DEFAULT NULL,
+  `step5_status` varchar(255) NOT NULL,
+  `step5_date` datetime DEFAULT NULL,
+  `ext1` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+ALTER TABLE `admissionstudent_enterencestep`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `admissionstudent_enterencestep`
+  ADD PRIMARY KEY (`id`);
 -- --------------------------------------------------------
 
 --
