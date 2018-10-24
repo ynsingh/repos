51aae8f5d6c29a3a3882cafd8383c334c48c1491
ynsingh <ynsingh@0c22728b-0eb9-4c4e-a44d-29de7e55569f>
2018-10-24 11:40:38 +0000

    extra project created

--- brihaspati/trunk/b4server/b4server.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS otp;
-create table otp (  otp VARCHAR(8), emailid varchar(25), ccert blob, primary key (emailid));
-DROP TABLE IF EXISTS keystore;
-create table keystore (  nodeid blob, emailid varchar(25), privkey blob, pubkey varbinary(10000),  validdays long, organisationalunit varchar(50) , organisation varchar(50) , city varchar(50), state varchar(50), country varchar(50), alias varchar(50), keypass varchar(50), primary  key (emailid));
